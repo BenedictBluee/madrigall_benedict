9059458334e2fca4a7d830fdf0404160643bd1d8
BenedictBluee <benedictmadrigal26@gmail.com>
2025-09-20 23:49:55 +0800

    First commit

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -3,6 +3,8 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 /**
  * Model: UserModel
+ * 
+ * Automatically generated via CLI.
  */
 class UserModel extends Model {
     protected $table = 'students';
